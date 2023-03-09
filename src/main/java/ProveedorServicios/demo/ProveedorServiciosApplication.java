f0b725015b7086cce861a52c49c2541b2b6b8c52
EstefyModoSabio <hernandezmonagasestefany@gmail.com>
2023-03-09 20:37:46 -0300

    First Commit

---
+++ src/main/java/ProveedorServicios/demo/ProveedorServiciosApplication.java
@@ -0,0 +1,13 @@
+package ProveedorServicios.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveedorServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveedorServiciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProveedorServicios/demo/ProveedorServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package ProveedorServicios.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedorServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
