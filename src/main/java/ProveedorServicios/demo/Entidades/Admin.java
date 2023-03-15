6d93f952b052894210a18139a894adf58a048e08
EstefyModoSabio <hernandezmonagasestefany@gmail.com>
2023-03-14 21:24:30 -0300

    Version actualizada con todas las entidades en la DB

--- src/main/java/ProveedorServicios/demo/Entidades/Admin.java
+++ src/main/java/ProveedorServicios/demo/Entidades/Admin.java
@@ -1,33 +1,86 @@
-
 package ProveedorServicios.demo.Entidades;
 
+import ProveedorServicios.demo.Enums.Rol;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-public class Admin extends Usuario{
-    
+public class Admin {
+
     @Id
-    @GeneratedValue(generator = "system=uuid")
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private String Barrio;
-    private String hola;
+    private String dni;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String password;
+    private String email;
+    private Rol rol;
 
     public Admin() {
     }
 
-    public String getHola() {
-        return hola;
+    public String getDni() {
+        return dni;
     }
 
-    public void setHola(String hola) {
-        this.hola = hola;
+    public void setDni(String dni) {
+        this.dni = dni;
     }
-     
 
-    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
     public String getId() {
         return id;
     }
@@ -43,7 +96,5 @@ public class Admin extends Usuario{
     public void setBarrio(String Barrio) {
         this.Barrio = Barrio;
     }
-     
-     
-    
+
 }
